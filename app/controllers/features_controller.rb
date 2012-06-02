e0dd51a9593e1fffa36ee41743379b8798cc8e4e
Joe Peck <fatcatt316@yahoo.com>
2012-06-02 15:11:51 -0400

    Formatting/indentation changes
    Removing unnecessary code, as well.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,6 +1,5 @@
 class FeaturesController < ApplicationController
 
-
   def index
     @features = Feature.all
   end
@@ -31,31 +30,21 @@ class FeaturesController < ApplicationController
     end
   end
 
-  # PUT /features/1
-  # PUT /features/1.xml
+
   def update
     @feature = Feature.find(params[:id])
 
-    respond_to do |format|
-      if @feature.update_attributes(params[:feature])
-        format.html { redirect_to(@feature, :notice => 'Feature was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @feature.errors, :status => :unprocessable_entity }
-      end
+    if @feature.update_attributes(params[:feature])
+      redirect_to(@feature, :notice => 'Feature was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /features/1
-  # DELETE /features/1.xml
+
   def destroy
     @feature = Feature.find(params[:id])
     @feature.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(features_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(features_url)
   end
 end
