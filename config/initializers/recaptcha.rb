6070f323900050245a74f402e88393ab7f295e70
Joe Peck <joe.peck@scimedsolutions.com>
2012-11-05 22:23:46 -0500

    Whoops, fixed development recaptcha keys for real this time

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
   if Rails.env.development?
-    config.public_key  = '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy'
-    config.private_key = '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx'
+    config.public_key  = '6LeR4c0SAAAAAHWD6s8eP1BJdSA15xd_p6uHNf2F'
+    config.private_key = '6LeR4c0SAAAAAOLdNWuFaMN9gDqAqgztMFnm2uIl'
   end
 end
