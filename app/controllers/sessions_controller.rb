5e7a3268f662c91992d93e63716441d7d698b2b8
Joe Peck <joe.peck@scimedsolutions.com>
2012-11-05 20:42:59 -0500

    Fix login from new ad form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,28 +8,15 @@ class SessionsController < ApplicationController
     user = login(params[:email], params[:password], params[:remember_me])
     
     respond_to do |format|
-      if user
-        format.html { redirect_back_or_to root_url, :notice => "Logged in!" }
-        
-        ## TODO: Get this part to work!
-        format.js do
-          render :update do |page|
-            page << "jQuery('#absent_user').html('<strong>You\'re logged in and good to go!</strong>');"
-            page << "jQuery('#not_signed_in_warning').hide();"
-            page.replace_html('upper_login', :partial => 'sessions/form')
-          end
-        end
-      else
-        format.html {
+      format.html do
+        if user
+          redirect_back_or_to root_url, :notice => "Logged in!"
+        else
           flash.now[:warning] = "Email or password was invalid."
           render :new
-        }
-        format.js do
-          render :update do |page|
-            page << "jQuery('#login_error_message').show().html('Email or password was invalid.');"
-          end
         end
       end
+      format.js
     end
   end
   
