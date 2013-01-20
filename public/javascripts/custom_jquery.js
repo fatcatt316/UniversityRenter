be275e7524d4dcb393ea8af93f5e800348d51328
Joe Peck <joe.peck@scimedsolutions.com>
2013-01-20 10:50:10 -0500

    Replace hard tabs and old hash rockets

--- public/javascripts/custom_jquery.js
+++ public/javascripts/custom_jquery.js
@@ -1,12 +1,12 @@
 // TODO: Change "jQuery" to "$"
 jQuery(document).ready(function() {
-	
-	// Used on listings/community_college_picker
-	// TODO: Consider loading this JS only on pages that use this partial.
-	jQuery('select#state_id').change(function(){
-		var state_id = jQuery(this).val();
+  
+  // Used on listings/community_college_picker
+  // TODO: Consider loading this JS only on pages that use this partial.
+  jQuery('select#state_id').change(function(){
+    var state_id = jQuery(this).val();
     jQuery.ajax({
-	    url: '/addresses/select_state',
+      url: '/addresses/select_state',
       data: 'state_id=' + state_id
     });
   });
@@ -16,36 +16,36 @@ jQuery(document).ready(function() {
   });
 
   jQuery('select#city').live("change", function(){
-		var state_id = jQuery('select#state_id').val();
-		var city = jQuery(this).val();
-		
-		console.log("state_id:" + state_id + " city:" + city);
+    var state_id = jQuery('select#state_id').val();
+    var city = jQuery(this).val();
+    
+    console.log("state_id:" + state_id + " city:" + city);
     jQuery.ajax({
-	    url: '/addresses/select_city',
+      url: '/addresses/select_city',
       data: 'city=' + city + '&state_id=' + state_id
     });
   });
 
-	
-	// Used on listings/_form
-	// TODO: Consider loading this only on pages that use this partial.
-	var selected_id = jQuery("input[name=\"listing[wanted]\"]:checked").attr('id');
-	
-	jQuery('#listing_wanted_false').change(function() {
-		jQuery('.wanted :input').attr('disabled', true)
-	  jQuery(".wanted").hide();
-		jQuery(".for_rent :input").removeAttr('disabled');
-	  jQuery(".for_rent").show();
-	});
-		
-	jQuery('#listing_wanted_true').change(function() {
-	  jQuery('.for_rent :input').attr('disabled', true)
-	  jQuery(".for_rent").hide();
-		jQuery(".wanted :input").removeAttr('disabled');
-		jQuery(".wanted").show();
-	});
-	
-	jQuery('#' + selected_id).change();
+  
+  // Used on listings/_form
+  // TODO: Consider loading this only on pages that use this partial.
+  var selected_id = jQuery("input[name=\"listing[wanted]\"]:checked").attr('id');
+  
+  jQuery('#listing_wanted_false').change(function() {
+    jQuery('.wanted :input').attr('disabled', true)
+    jQuery(".wanted").hide();
+    jQuery(".for_rent :input").removeAttr('disabled');
+    jQuery(".for_rent").show();
+  });
+    
+  jQuery('#listing_wanted_true').change(function() {
+    jQuery('.for_rent :input').attr('disabled', true)
+    jQuery(".for_rent").hide();
+    jQuery(".wanted :input").removeAttr('disabled');
+    jQuery(".wanted").show();
+  });
+  
+  jQuery('#' + selected_id).change();
 });
 
 jQuery.noConflict();
