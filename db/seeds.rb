84f2dfab4c3a60547a3aab0dd119031ef4d84567
Joe Peck <joe.peck@scimedsolutions.com>
2013-02-09 20:30:21 -0500

    Remove ids from seeds for Colleges

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,35 +9,35 @@
 nc_state = State.find_by_name("North Carolina")
 college_info = [
   # NC State
-  {id: 1, name: "NC State", short_name: "NCSU", mascot: "Wolfpack", description: "NC State University is located near downtown Raleigh.  With over 30,000 students, there are many student apartments right off NCSU's campus in Raleigh.",
+  {name: "NC State", short_name: "NCSU", mascot: "Wolfpack", description: "NC State University is located near downtown Raleigh.  With over 30,000 students, there are many student apartments right off NCSU's campus in Raleigh.",
   address_attributes: {line1: "100 Hillsborough Street", zip: "27606", city: "Raleigh", state_id: nc_state.id}},
   
   # UNC
-  {id: 2, name: "UNC Chapel Hill", short_name: "UNC", mascot: "Tarheels", description: "University of North Carolina Chapel Hill is located, surprising enough, in Chapel Hill, North Carolina.  Opened way back in 1795, it was the first public college in the United States o' America.",
+  {name: "UNC Chapel Hill", short_name: "UNC", mascot: "Tarheels", description: "University of North Carolina Chapel Hill is located, surprising enough, in Chapel Hill, North Carolina.  Opened way back in 1795, it was the first public college in the United States o' America.",
   address_attributes: {line1: "209 South Road", zip: "27599", city: "Chapel Hill", state_id: nc_state.id}},
   
   # UNCC
-  {id: 3, name: "UNC Charlotte", short_name: "UNCC", mascot: "49ers", description: "UNC Charlotte is is located in the Queen City, which has the largest population of any city in North Carolina.  The number of off-campus apartments around UNCC truly boggles the mind.",
+  {name: "UNC Charlotte", short_name: "UNCC", mascot: "49ers", description: "UNC Charlotte is is located in the Queen City, which has the largest population of any city in North Carolina.  The number of off-campus apartments around UNCC truly boggles the mind.",
   address_attributes: {line1: "9201 University City Blvd", zip: "28223", city: "Charlotte", state_id: nc_state.id}},
   
   # UNCW
-  {id: 4, name: "UNC Wilmington", short_name: "UNCW", mascot: "Seahawks", description: "Surfs up!  Situated near the beautiful North Carolina coast, UNCW offers higher education with all the benefits of being close to the beach.",
+  {name: "UNC Wilmington", short_name: "UNCW", mascot: "Seahawks", description: "Surfs up!  Situated near the beautiful North Carolina coast, UNCW offers higher education with all the benefits of being close to the beach.",
   address_attributes: {line1: "601 S College Rd", zip: "28403", city: "Wilmington", state_id: nc_state.id}},
   
   # ECU
-  {id: 5, name: "Eastern Carolina University", short_name: "ECU", mascot: "Pirates", description: "With the motto \"Tomorrow Starts Here\", ECU ambitiously puts its best foot forward.  Located in Greenville, NC, it was founded back in 1907, making it a wee bit over 100 years old.",
+  {name: "Eastern Carolina University", short_name: "ECU", mascot: "Pirates", description: "With the motto \"Tomorrow Starts Here\", ECU ambitiously puts its best foot forward.  Located in Greenville, NC, it was founded back in 1907, making it a wee bit over 100 years old.",
   address_attributes: {line1: "East Fifth Street", zip: "27858", city: "Greenville", state_id: nc_state.id}},
   
   # ASU
-  {id: 6, name: "App State", short_name: "ASU", mascot: "Mountaineers", description: "Nestled in the gorgeous mountain town of Boone, ASU offers an excellent college education along with an amazing natural setting.",
+  {name: "App State", short_name: "ASU", mascot: "Mountaineers", description: "Nestled in the gorgeous mountain town of Boone, ASU offers an excellent college education along with an amazing natural setting.",
   address_attributes: {line1: "287 Rivers St", zip: "28608", city: "Boone", state_id: nc_state.id}},
   
   # UNCG
-  {id: 7, name: "UNC Greensboro", short_name: "UNCG", mascot: "Spartans", description: "Right in the middle of the thriving city of Greensboro, UNCG started off with 198 students in 1892.  In the 120+ years since, it's grown quite a bit.",
+  {name: "UNC Greensboro", short_name: "UNCG", mascot: "Spartans", description: "Right in the middle of the thriving city of Greensboro, UNCG started off with 198 students in 1892.  In the 120+ years since, it's grown quite a bit.",
   address_attributes: {line1: "1400 Spring Garden St", zip: "27412", city: "Greensboro", state_id: nc_state.id}},
   
   # Duke
-  {id: 8, name: "Duke University", short_name: "Duke", mascot: "Wolfpack", description: "Duke is a private university situated in Durham, AKA the Bull City.  In 2013, \"U.S. News & World Report\" ranked Duke's undergrad program 8th best in the whole United States.",
+  {name: "Duke University", short_name: "Duke", mascot: "Wolfpack", description: "Duke is a private university situated in Durham, AKA the Bull City.  In 2013, \"U.S. News & World Report\" ranked Duke's undergrad program 8th best in the whole United States.",
   address_attributes: {line1: "2138 Campus Drive", zip: "27705", city: "Durham", state_id: nc_state.id}}
 ]
 
