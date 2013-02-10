68af97067fb531f17ecbe047560de5ae77c403c9
Joe Peck <joe.peck@scimedsolutions.com>
2013-02-09 20:35:17 -0500

    Switching order in listings header

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -3,7 +3,7 @@ module ListingsHelper
     header = ""
     header << "#{current_college.address.city} " if current_college
     header << "Student Apartments"
-    header << " near #{current_college}" if current_college
+    header << " near #{current_college.short_name}" if current_college
     return header
   end
 end
