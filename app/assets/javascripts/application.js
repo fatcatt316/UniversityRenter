6dfba5e00abc9e7b5bc58c8d7ddeb8223ba7d1ad
Joe Peck <joe.peck@scimedsolutions.com>
2013-05-13 20:39:39 -0400

    Move custom_jquery into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,67 @@ function add_fields(link, association, content, where) {
 }
 
 /////// END Nested forms END //////////
+
+
+// TODO: Change "jQuery" to "$"
+jQuery(document).ready(function() {
+  
+  // Used on listings/community_college_picker
+  // TODO: Consider loading this JS only on pages that use this partial.
+  jQuery('select#state_id').change(function(){
+    var state_id = jQuery(this).val();
+    jQuery.ajax({
+      url: '/addresses/select_state',
+      data: 'state_id=' + state_id
+    });
+  });
+  
+  jQuery('select.college_dropdown').live('change', function() {
+    jQuery.ajax({
+      url: '/communities/update_dropdown',
+      data: 'college_id=' + this.value
+    });
+  });
+
+  jQuery('.datepicker').live('focus', function () {
+    jQuery(this).not('.hasDatePicker').datepicker();
+  });
+
+  jQuery('select#city').live("change", function(){
+    var state_id = jQuery('select#state_id').val();
+    var city = jQuery(this).val();
+    
+    // console.log("state_id:" + state_id + " city:" + city);
+    jQuery.ajax({
+      url: '/addresses/select_city',
+      data: 'city=' + city + '&state_id=' + state_id
+    });
+  });
+
+  
+  // Used on listings/_form
+  // TODO: Consider loading this only on pages that use this partial.
+  var selected_id = jQuery("input[name=\"listing[wanted]\"]:checked").attr('id');
+  
+  jQuery('#listing_wanted_false').change(function() {
+    jQuery('#wanted_label').removeClass("chosen_label");
+    jQuery('#for_rent_label').addClass("chosen_label");
+    jQuery('.wanted :input').attr('disabled', true);
+    jQuery(".wanted").hide();
+    jQuery(".for_rent :input").removeAttr('disabled');
+    jQuery(".for_rent").show();
+  });
+    
+  jQuery('#listing_wanted_true').change(function() {
+    jQuery('#for_rent_label').removeClass("chosen_label");
+    jQuery('#wanted_label').addClass("chosen_label");
+    jQuery('.for_rent :input').attr('disabled', true);
+    jQuery(".for_rent").hide();
+    jQuery(".wanted :input").removeAttr('disabled');
+    jQuery(".wanted").show();
+  });
+  
+  jQuery('#' + selected_id).change();
+});
+
+jQuery.noConflict();
