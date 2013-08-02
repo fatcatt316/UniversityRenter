f9455f05f621ee0129cf77d0892a3e7821d54264
Joe Peck <joe.peck@scimedsolutions.com>
2013-08-01 21:15:41 -0400

    Fix some deprecation warnings

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,5 +1,5 @@
 class College < ActiveRecord::Base
-  has_many :communities, order: :name, dependent: :destroy
+  has_many :communities, -> { order :name }, dependent: :destroy
   has_many :listings, dependent: :destroy
   
   has_one :address, as: :subject, dependent: :destroy
