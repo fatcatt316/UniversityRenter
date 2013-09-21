b7166c5fdfcc12bf30838a20e30617aa863b648b
Joe Peck <joe.peck@scimedsolutions.com>
2013-09-21 13:38:34 -0400

    Moving S3 paperclip settings into document

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,14 @@ class Document < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :primary, :scope => [:owner_id, :owner_type], :if => :primary?
   
-  has_attached_file :doc, 
+  has_attached_file :doc,
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
     :styles => { :medium => "200x200>", :thumb => "70x70#" }, # use # to make it at least that size
     :default_style => :medium
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,4 @@ UniversityRenter::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-  # Set paperclip to use AWS
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    :path => '/:class/:attachment/:id_partition/:style/:filename'
-  }
 end
