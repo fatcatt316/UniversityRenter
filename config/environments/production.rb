18611b462b2a1dc816a83d2de918aed909638b0f
Joe Peck <joe.peck@scimedsolutions.com>
2013-09-21 12:34:52 -0400

    Learning how to hash rocket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ UniversityRenter::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url = ':s3_domain_url',
-    :path = '/:class/:attachment/:id_partition/:style/:filename'
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
