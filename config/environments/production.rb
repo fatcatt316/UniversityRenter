6c65881d8d34d8ecea967e04b73bc84423780e32
Joe Peck <joe.peck@scimedsolutions.com>
2013-09-21 12:32:28 -0400

    Trying to reconnect AWS to Paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ UniversityRenter::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url = ':s3_domain_url',
+    :path = '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
