51bea6be2d9d417c927d646b86806d674b7f9c94
Joe Peck <joe.peck@scimedsolutions.com>
2013-09-21 12:48:28 -0400

    Non-functional changes in document

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,33 +6,28 @@ class Document < ActiveRecord::Base
   has_attached_file :doc, 
     :styles => { :medium => "200x200>", :thumb => "70x70#" }, # use # to make it at least that size
     :default_style => :medium
-  #  :url  => "/avatars/:id/:style/:basename.:extension", #place in public
-  #  :default_url => "missing.png"#,
-    #:path => ":rails_root/public/avatars/:id/:style/:basename.:extension"
+
   validates_attachment_presence :doc
   validates_attachment_size :doc, :less_than => 5.megabytes
-  validates_attachment_content_type :doc, :content_type => ['image/jpeg', 'image/png', 'image/gif',
-    'image/pjpeg', 'image/x-png'], 
+  validates_attachment_content_type :doc, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'image/pjpeg', 'image/x-png'],
     :message => "must be one of these image types: JPG, PNG, or GIF"    
   
   before_post_process :image?
 
   
   def to_s
-    return title ? title : doc_file_name
+    title ? title : doc_file_name
   end
-  
   
   def image?
     !(doc_content_type.to_s =~ /^image.*/).nil?
   end
   
-  
   def thumbnail_url
-    return image? ? doc.url(:thumb) : "document_50x50.gif"
+    image? ? doc.url(:thumb) : "document_50x50.gif"
   end
   
   def normal_url
-    return image? ? doc.url(:medium) : "document_100x100.gif"
+    image? ? doc.url(:medium) : "document_100x100.gif"
   end
 end
