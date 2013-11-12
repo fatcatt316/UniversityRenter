d044400e87a6026d06ff599c0a657822f5b78353
Joe Peck <joe.peck@scimedsolutions.com>
2013-11-11 22:34:45 -0500

    Add listing icon (for wanted or for rent)

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -14,4 +14,12 @@ module ListingsHelper
   def listing_class(listing)
     "listing-pending" if listing.pending?
   end
+  
+  def listing_icon(listing)
+    if listing.wanted?
+      raw("<span class='listing-icon wanted'>Room Wanted</span>")
+    else
+      raw("<span class='listing-icon for-rent'>For Rent</span>")
+    end
+  end
 end
